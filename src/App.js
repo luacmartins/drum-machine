6fdabd566d9d00a2f65aba4d1eb300ed204bcb9e
Lua Carlos Martins <luacmartins@Luas-MacBook-Pro.local>
2020-10-09 15:04:47 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { pads } from './audio'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [audio, setAudio] = useState('')
+   const [power, setPower] = useState(false)
+
+   const handleClick = (pad) => {
+      if (!power) return
+      setAudio(pad.audioName)
+      pad.audio.play()
+   }
+
+   const turnOn = () => {
+      power ? setAudio('') : setAudio('---')
+      setPower(!power)
+   }
+   const playSound = (e) => {
+      pads.forEach(pad => {
+         if (!power) return
+         if (e.key === pad.key.toLocaleLowerCase()) {
+            pad.audio.play()
+            setAudio(pad.audioName)
+         }
+      })
+   }
+
+   useEffect(() => {
+      document.addEventListener('keydown', playSound)
+      return function cleanup() {
+         document.removeEventListener('keydown', playSound)
+      }
+   }, [power])
+
+   return (
+      <div id="drum-machine" className="App">
+         <label htmlFor="power">
+            <input type="checkbox" name="power" id="power" onChange={turnOn} checked={power} />
+            <span><i className={`${power ? 'on ' : ''}fas fa-power-off`}></i></span>
+         </label>
+         <div id="display">{audio}</div>
+         <div className="pads-container">
+            {pads.map(pad => (
+               <div key={pad.key} id={pad.audioName} className='drum-pad' onClick={() => handleClick(pad)}>
+                  <audio src={pad.audio} id={pad.key} className="clip"></audio>
+                  {pad.key}
+               </div>
+            ))}
+         </div>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/audio.js
@@ -0,0 +1,37 @@
+export const pads = [{
+   key: 'Q',
+   audioName: 'Heater-1',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3')
+}, {
+   key: 'W',
+   audioName: 'Heater-2',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3')
+}, {
+   key: 'E',
+   audioName: 'Heater-3',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3')
+}, {
+   key: 'A',
+   audioName: 'Heater-4',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3')
+}, {
+   key: 'S',
+   audioName: 'Heater-5',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3')
+}, {
+   key: 'D',
+   audioName: 'Open-HH',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3')
+}, {
+   key: 'Z',
+   audioName: "Kick-n'-Hat",
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3')
+}, {
+   key: 'X',
+   audioName: 'Kick',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3')
+}, {
+   key: 'C',
+   audioName: 'Closed-HH',
+   audio: new Audio('https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3')
+}]
